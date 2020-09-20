676bcf292c8012e030983920790276a8e704c7d6
Andrew Raftovich <AndrewRaftovich@gmail.com>
2020-09-20 14:50:52 -0700

    Added Page Navigator (#1)

--- src/App.js
+++ src/App.js
@@ -1,77 +1,29 @@
-import React, { useState } from 'react';
-import { FlatList, StyleSheet, TextInput, View, Button } from 'react-native';
+import React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
 
-import Message from './components/message';
+import MessageView from './views/MessageView';
 
-export default function App() {
-
-  const [enteredText, setEnteredText] = useState('');
-  const [messageList, setMessageList] = useState([]);
-  //const [msgIndex, setMsgIndex] = useState(1);
-  //const [messageRef, setmessageRef] = useState(undefined);
-
-  let index = 1;
-  let messageRef = undefined;
-
-  const goalInputHandler = enteredText => {
-    setEnteredText(enteredText);
-  }
-
-  const addGoalHandler = () => {
-    setMessageList(currentGoals => [
-      ...currentGoals,
-      { id: index++, value: enteredText }
-    ]);
-    messageRef.scrollToEnd({animated: true})
-  };
-
+function LoginScreen() {
   return (
-    <View style={styles.screen}>
-      <FlatList
-        ref={ref => messageRef = ref}
-        onContentSizeChange={() => {
-          messageRef.scrollToEnd({ animated: true });
-        }}
-        keyExtractor={(item, index) => `item: ${item}, index: ${index}`}
-        data={messageList}
-        renderItem={itemData => <Message content={itemData.item.value} /> }
-      />
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Message"
-          onChangeText={goalInputHandler}
-          value={enteredText}
-        />
-        <Button
-          style={styles.container}
-          title="ADD"
-          onPress={addGoalHandler}
-        />
-      </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Page!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  screen: {
-    padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    width: '80%',
-    borderColor: 'black',
-    borderWidth: 1,
-    padding: 10
-  },
-});
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MessageView} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
