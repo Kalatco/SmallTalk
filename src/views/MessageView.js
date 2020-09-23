43cbc15b73f779d716c49fe10769c9073e66f07b
Robbie Bagley <39345139+kansairob@users.noreply.github.com>
2020-09-22 19:10:57 -0700

    Fixed message component (#2)

--- src/views/MessageView.js
+++ src/views/MessageView.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
-import { FlatList, StyleSheet, TextInput, View, Button } from 'react-native';
-import Message from './../components/Message';
-
+import React, { useState } from "react";
+import { FlatList, StyleSheet, TextInput, View, Button } from "react-native";
+import Message from "./../components/message";
 
 export default function MessageView() {
-  const [enteredText, setEnteredText] = useState('');
+  const [enteredText, setEnteredText] = useState("");
   const [messageList, setMessageList] = useState([]);
   //const [msgIndex, setMsgIndex] = useState(1);
   //const [messageRef, setmessageRef] = useState(undefined);
@@ -12,28 +11,28 @@ export default function MessageView() {
   let index = 1;
   let messageRef = undefined;
 
-  const goalInputHandler = enteredText => {
+  const goalInputHandler = (enteredText) => {
     setEnteredText(enteredText);
-  }
+  };
 
   const addGoalHandler = () => {
-    setMessageList(currentGoals => [
+    setMessageList((currentGoals) => [
       ...currentGoals,
-      { id: index++, value: enteredText }
+      { id: index++, value: enteredText },
     ]);
-    messageRef.scrollToEnd({animated: true})
+    messageRef.scrollToEnd({ animated: true });
   };
 
   return (
     <View style={styles.screen}>
       <FlatList
-        ref={ref => messageRef = ref}
+        ref={(ref) => (messageRef = ref)}
         onContentSizeChange={() => {
           messageRef.scrollToEnd({ animated: true });
         }}
         keyExtractor={(item, index) => `item: ${item}, index: ${index}`}
         data={messageList}
-        renderItem={itemData => <Message content={itemData.item.value} /> }
+        renderItem={(itemData) => <Message content={itemData.item.value} />}
       />
       <View style={styles.inputContainer}>
         <TextInput
@@ -42,11 +41,7 @@ export default function MessageView() {
           onChangeText={goalInputHandler}
           value={enteredText}
         />
-        <Button
-          style={styles.container}
-          title="ADD"
-          onPress={addGoalHandler}
-        />
+        <Button style={styles.container} title="ADD" onPress={addGoalHandler} />
       </View>
     </View>
   );
@@ -55,22 +50,22 @@ export default function MessageView() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   screen: {
-    padding: 50
+    padding: 50,
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   input: {
-    width: '80%',
-    borderColor: 'black',
+    width: "80%",
+    borderColor: "black",
     borderWidth: 1,
-    padding: 10
+    padding: 10,
   },
 });
