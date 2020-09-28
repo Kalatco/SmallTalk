4612933afc7d9fe8715fd11bcec934aab30f2a7d
Robbie Bagley <rbagley01@gmail.com>
2020-09-28 14:17:26 -0700

    Simplified the login view

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from "react";
-import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import MessageView from "./views/MessageView";
-
-function LoginScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Login Page!</Text>
-    </View>
-  );
-}
+import LoginScreen from "./views/LoginView";
 
 const Stack = createStackNavigator();
 
