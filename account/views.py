fd82c6f4b4c5ab8827bed20855d924f5b2bf3999
kalatco <andrewraftovich@gmail.com>
2020-09-27 22:47:05 -0700

    rebuilt registration process

--- account/views.py
+++ account/views.py
@@ -1,51 +1,18 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from account.forms import RegistrationForm, AccountAuthenticationForm
-from django.http import HttpResponse, JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+from account.serializers import RegistrationSerializer
+
+@api_view(['POST'])
 def registration_view(request):
-    if request.POST:
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-
-            email = form.cleaned_data.get('email')
-            raw_password = form.cleaned_data.get('password1')
-            account = authenticate(email=email, password=raw_password)
-            login(request, account)
-            return JsonResponse({ 'message': 'account created!' })
-        return JsonResponse({ 'message': 'Invalid input' })
-    return JsonResponse({ 'message', 'request type not supported, must be POST'})
-
-
-def login_view(request):
-    user = request.user
-    next = request.GET.get('next')
-
-    if user.is_authenticated:
-        if next:
-            return redirect(next)
-        return JsonResponse({ 'message': 'logged in already' })
-    
-    elif request.POST:
-        form = AccountAuthenticationForm(request.POST)
-
-        if form.is_valid():
-            email = request.POST['email']
-            password = request.POST['password']
-            user = authenticate(email=email, password=password)
-
-            if user:
-                login(request, user)
-
-                if next:
-                    return redirect(next)
-                    return JsonResponse({ 'message': 'logged in!' })
-        return JsonResponse({ 'message': 'Invalid input' })
+    serializer = RegistrationSerializer(data=request.data)
+    data = {}
+    if serializer.is_valid():
+        account = serializer.save()
+        data['response'] = "new account created"
+        data['email'] = account.email
+        data['username'] = account.username
     else:
-        return JsonResponse({ 'message': 'not logged in' })
-
-
-def logout_view(request):
-    logout(request)
-    return JsonResponse({ 'message': 'logged out' })
+        data = serializer.errors
+    return Response(data)
--- smallTalk/urls.py
+++ smallTalk/urls.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 
+'''
 from account.views import (
     registration_view,
     login_view,
     logout_view,
 )
+'''
+from account.views import (
+    registration_view,
+)
 
 urlpatterns = [
     path('messanger/', include('messanger.urls', 'messanger_api')),
     path('admin/', admin.site.urls),
-    path('register', registration_view),
-    path('login', login_view),
-    path('logout', logout_view),
+    path('register', registration_view, name="register")
 ]
