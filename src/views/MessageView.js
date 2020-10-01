1efca2fb1b137fb87e9a7e77cf935787411659a0
kalatco <andrewraftovich@gmail.com>
2020-09-30 18:11:06 -0700

    Added fake data used for implementing functionality locally

--- src/views/MessageView.js
+++ src/views/MessageView.js
@@ -3,6 +3,84 @@ import { FlatList, StyleSheet, TextInput, View, Button } from "react-native";
 import Message from "./../components/message";
 import {connect} from 'react-redux';
 
+/**
+ * Fake API Call
+ * 
+ * This JavaScript Object (In Json format) represents the data that a server
+ * can potentially send to this phone app or anywhere. So in the code below, 
+ * we have a Chats and a Users API call data, The programmer can access the data 
+ * inside and display it on the screen, depending on which information they
+ * need to show to the user. For example, A user looking at Chat with id #2,
+ * would not want to see data made for Chat #1, each message contains a User ID,
+ * so the message would need to access the API to see the username of who
+ * send the message.
+ */
+const FAKE_API_CALL = {
+  chat_messages_api_call: [
+    {
+      chatId: 1,
+      messages: [
+        {
+          messageId: 1,
+          message: "first message!",
+          authorId: 1,
+          created: "2020-09-27 00:02:31",
+        },
+        {
+          messageId: 2,
+          message: "second message!",
+          authorId: 2,
+          created: "2020-09-27 00:02:31",
+        },
+        {
+          messageId: 3,
+          message: "another message!",
+          authorId: 1,
+          created: "2020-09-27 00:02:31",
+        },
+      ],
+    },
+    {
+      chatId: 2,
+      messages: [
+        {
+          messageId: 4,
+          message: "first message! of the second chat",
+          authorId: 1,
+          created: "2020-09-27 00:02:31",
+        },
+        {
+          messageId: 5,
+          message: "second message!",
+          authorId: 2,
+          created: "2020-09-27 00:02:31",
+        },
+        {
+          messageId: 6,
+          message: "first message!",
+          authorId: 2,
+          created: "2020-09-27 00:02:31",
+        },
+      ],
+    },
+  ],
+  users_api_call: [
+    {
+      userId: 1,
+      username: "firstUser",
+    },
+    {
+      userId: 2,
+      username: "secondUser",
+    },
+    {
+      userId: 3,
+      username: "thirdUser",
+    },
+  ],
+};
+
+
 function MessageView(props) {
   const [enteredText, setEnteredText] = useState("");
   const [messageRef, setmessageRef] = useState(undefined);
