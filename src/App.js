38586513deff60e99d94870621b3f60ceba30dc3
Andrew Raftovich <AndrewRaftovich@gmail.com>
2020-10-02 23:24:23 -0700

    Added navigation between login, messages, sidemenu, & settings (#8)
    @kansairob helped with setting up navigator

--- src/App.js
+++ src/App.js
@@ -1,32 +1,120 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { Button, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Provider } from 'react-redux';
 import { store } from './store/index';
+
+import SigninView from './views/LoginView';
+import SettingsView from './views/SettingsView';
 import MessageView from './views/MessageView';
+import ChatSelection from './views/ChatSelection';
 
-function LoginScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Login Page!</Text>
-    </View>
-  );
-}
+/*
+  Stacks - each one contains a pointer to a view
+*/
 
-const Stack = createStackNavigator();
+const LoginStack = createStackNavigator();
+const LoginStackScreen = () => (
+  <LoginStack.Navigator>
+    <LoginStack.Screen
+      name="Signin"
+      component={SigninView}
+      options={{ title: "Sign In" }}
+    />
+  </LoginStack.Navigator>
+);
 
-function App() {
+// Message Screen has a child Settings page
+const MessageStack = createStackNavigator();
+const MessageStackScreen = () => (
+  <MessageStack.Navigator>
+    <MessageStack.Screen
+      name="Messages"
+      component={MessageView}
+      options={({ navigation }) => ({
+        headerTitle: () => (
+          <Text style={{
+            textAlign: 'center',
+            alignSelf:'center',
+            fontSize: 24,
+          }}>
+            Messages
+          </Text>
+        ),
+        headerLeft: () => (
+          <Button
+            title="Side menu"
+            onPress={() => navigation.toggleDrawer()}
+          />
+        ),
+        headerRight: () => (
+          <Button
+            title="Settings"
+            onPress={() => navigation.push("Settings", { name: "Settings" }) }
+          />
+        ),
+      })}
+      />
+    <MessageStack.Screen
+      name="Settings"
+      component={SettingsView}
+      options={({ route }) => ({
+        title: route.params.name,
+      })}
+    />
+  </MessageStack.Navigator>
+);
+
+// Sidemenu contains the message screen stack component
+const SideMenuStack = createDrawerNavigator();
+const SideMenuScreen = () => (
+  <SideMenuStack.Navigator drawerContent={ChatSelection}>
+    <SideMenuStack.Screen name="MessageAndSettings" component={MessageStackScreen} />
+  </SideMenuStack.Navigator>
+);
+
+/*
+  Parent Stack - entry point to either login or message view
+*/
+
+// toggle to either see login page or see messages page
+const RENDER_LOGIN_PAGE = true;
+
+const RootStack = createStackNavigator();
+const RootStackScreen = () => (
+  <RootStack.Navigator headerMode="none">
+    {RENDER_LOGIN_PAGE ? (
+      <RootStack.Screen
+        name="Home"
+        component={SideMenuScreen}
+        options={{
+          animationEnabled: false,
+        }}
+      />
+    ) : (
+      <RootStack.Screen
+        name="Login"
+        component={LoginStackScreen}
+        options={{
+          animationEnabled: false,
+        }}
+      />
+    )}
+  </RootStack.Navigator>
+);
+
+/*
+  Default - renders all the stacks
+*/
+
+export default () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={MessageView} />
-          <Stack.Screen name="Login" component={LoginScreen} />
-        </Stack.Navigator>
+        <RootStackScreen />
       </NavigationContainer>
     </Provider>
   );
-}
-
-export default App;
+};
---
+++ src/views/SettingsView.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function SettingsView() {
+  return (
+    <View style={styles.container}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+  },
+});
