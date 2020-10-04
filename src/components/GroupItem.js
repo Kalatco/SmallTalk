3051ed4d6e6760c9bf7da694f9142ea4849ad6fb
KazyManazy <KazyManazy>
2020-10-04 13:24:53 -0700

    Added the settings screen

---
+++ src/components/GroupItem.js
@@ -0,0 +1,34 @@
+export const TestGroups = [
+  {
+    id: "1",
+    name: "group 1",
+  },
+  {
+    id: "2",
+    name: "group 2",
+  },
+  {
+    id: "3",
+    name: "group 3",
+  },
+  {
+    id: "4",
+    name: "group 4",
+  },
+  {
+    id: "5",
+    name: "group 5",
+  },
+  {
+    id: "6",
+    name: "group 6",
+  },
+  {
+    id: "7",
+    name: "group 7",
+  },
+];
+
+export default {
+  TestGroups,
+};
--- src/views/SettingsView.js
+++ src/views/SettingsView.js
@@ -1,24 +1,114 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet, TextInput, View, Button, Text } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { TestGroups } from "../components/GroupItem";
 
-export default function SettingsView() {
+const renderGroup = ({ item }) => {
+  return <Text style={settingsStyles.groupTextStyle}>{item.name}</Text>;
+};
+
+export default function SettingsView(props) {
   return (
-    <View style={styles.container}>
-      <Text>Settings Screen</Text>
-    </View>
+    <KeyboardAwareScrollView style={settingsStyles.container}>
+      <View style={settingsStyles.inputContainers}>
+        <Text style={settingsStyles.textStyle}>Username:</Text>
+        <TextInput style={settingsStyles.textInputStyle} />
+        {/* ADD JSX LATER TO GET USERNAME*/}
+      </View>
+      <View style={settingsStyles.inputContainers}>
+        <Text style={settingsStyles.textStyle}>First Name:</Text>
+        <TextInput style={settingsStyles.textInputStyle} />
+        {/* ADD JSX LATER TO GET FIRST NAME*/}
+      </View>
+      <View style={settingsStyles.inputContainers}>
+        <Text style={settingsStyles.textStyle}>Last Name:</Text>
+        <TextInput style={settingsStyles.textInputStyle} />
+        {/* ADD JSX LATER TO GET LAST NAME*/}
+      </View>
+      <View style={settingsStyles.inputContainers}>
+        <Text style={settingsStyles.textStyle}>Update Password:</Text>
+        <TextInput style={settingsStyles.textInputStyle} />
+        {/* ADD JSX LATER TO GET LAST NAME*/}
+      </View>
+      {/*START OF GROUPS VIEW*/}
+      <View style={settingsStyles.inputContainers}>
+        <Text style={settingsStyles.textStyle}>Current Groups:</Text>
+        <FlatList
+          style={settingsStyles.flatListStyle}
+          data={TestGroups}
+          renderItem={renderGroup}
+        />
+        <Text style={settingsStyles.textStyle}>New Group:</Text>
+        <TextInput
+          style={settingsStyles.textInputStyle}
+          onChangeText={() => console.log("New group text field changed.")}
+        />
+        {/* ADD JSX LATER TO GET LAST NAME*/}
+        <View style={settingsStyles.buttonContainer}>
+          <Button
+            color="gray"
+            title="Add New Group"
+            onPress={() =>
+              console.log("Add New Group button pressed! CHANGE LATER")
+            }
+          />
+          {/* ADD JSX LATER TO HANDLE ON PRESS*/}
+        </View>
+      </View>
+      <View style={settingsStyles.buttonContainer}>
+        <Button
+          color="gray"
+          title="Save Changes"
+          onPress={() => console.log("Save button pressed! CHANGE LATER")}
+        />
+        {/* ADD JSX LATER TO HANDLE ON PRESS*/}
+      </View>
+      <View style={settingsStyles.buttonContainer}>
+        <Button
+          color="gray"
+          title="Cancel"
+          onPress={() => console.log("Cancel button pressed! CHANGE LATER")}
+        />
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+const settingsStyles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "white",
   },
-  button: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    marginVertical: 10,
-    borderRadius: 5,
+  inputContainers: {
+    backgroundColor: "white",
+    alignItems: "center",
+    padding: 8,
+  },
+  buttonContainer: {
+    backgroundColor: "white",
+    alignItems: "center",
+    padding: 5,
+  },
+  textStyle: {
+    color: "black",
+    fontWeight: "bold",
+  },
+  groupTextStyle: {
+    color: "black",
+    fontWeight: "bold",
+  },
+  textInputStyle: {
+    height: 30,
+    width: 200,
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+  flatListStyle: {
+    backgroundColor: "gainsboro",
+    maxHeight: 60,
+    width: 200,
+    borderColor: "gray",
+    borderWidth: 2,
   },
 });
