4b0dea239f37a016097c3ded24d2258487b14e94
Robbie Bagley <39345139+kansairob@users.noreply.github.com>
2020-10-12 19:22:22 -0700

    Added login form (#17)
    * Edited the LoginView
    * Password can now be hidden when entered by user
    * added isValid flag

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,19 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
 // State variables
 const initialState = {
-
   // Temporary state variables, needs to be refactored
   index: 1,
   messageList: [],
 
   // State Variables for accessing content
   authenticationKey: "asdfabporijioajvdf94325-u2h8guo54p",
-  isSignedIn: true,
+  isSignedIn: false,
 
   // Selected chat
   selectedGroup: {
     groupId: 1,
-    chatId: 2, 
+    chatId: 2,
   },
 
   // Current user
@@ -64,14 +63,12 @@ const initialState = {
   },
 };
 
-
 // State Actions
-const reducer = (state=initialState, action) => {
-  switch(action.type) {
-
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     // Logging in
-    case 'SET_SIGNED_IN':
-      if(action.value) {
+    case "SET_SIGNED_IN":
+      if (action.value) {
         return {
           ...state,
           isSignedIn: action.value,
@@ -79,8 +76,8 @@ const reducer = (state=initialState, action) => {
       }
 
     // Settings
-    case 'SET_USERNAME':
-      if(action.value) {
+    case "SET_USERNAME":
+      if (action.value) {
         return {
           ...state,
           user: {
@@ -89,8 +86,8 @@ const reducer = (state=initialState, action) => {
           },
         };
       }
-    case 'SET_FIRSTNAME':
-      if(action.value) {
+    case "SET_FIRSTNAME":
+      if (action.value) {
         return {
           ...state,
           user: {
@@ -99,8 +96,8 @@ const reducer = (state=initialState, action) => {
           },
         };
       }
-    case 'SET_LASTNAME':
-      if(action.value) {
+    case "SET_LASTNAME":
+      if (action.value) {
         return {
           ...state,
           user: {
@@ -109,8 +106,8 @@ const reducer = (state=initialState, action) => {
           },
         };
       }
-    case 'SET_PASSWORD':
-      if(action.value) {
+    case "SET_PASSWORD":
+      if (action.value) {
         return {
           ...state,
           user: {
@@ -119,8 +116,8 @@ const reducer = (state=initialState, action) => {
           },
         };
       }
-    case 'ADD_GROUP':
-      if(action.value) {
+    case "ADD_GROUP":
+      if (action.value) {
         return {
           ...state,
           user: {
@@ -131,26 +128,26 @@ const reducer = (state=initialState, action) => {
                 name: action.value,
                 groupId: 0,
                 chats: [],
-              }
+              },
             ],
           },
         };
       }
 
     // TESTING
-    case 'PING':
-      console.log('Pong!');
+    case "PING":
+      console.log("Pong!");
       return state;
 
     // Messaging
-    case 'NEW_MESSAGE':
-      if(action.value) {
+    case "NEW_MESSAGE":
+      if (action.value) {
         return {
           ...state,
           messageList: [
             ...state.messageList,
-            { id: state.index++, value: action.value }
-          ], 
+            { id: state.index++, value: action.value },
+          ],
         };
       }
     default:
--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,26 +1,70 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { color } from "react-native-reanimated";
+import { Colors } from "react-native/Libraries/NewAppScreen";
 import { connect } from "react-redux";
 
 function SigninView(props) {
+  const [emailText, setEmailText] = useState("");
+  const handleEmailInput = (enteredText) => {
+    setEmailText(enteredText);
+  };
+
+  const [passwordText, setPasswordText] = useState("");
+  const handlePasswordInput = (enteredText) => {
+    setPasswordText(enteredText);
+  };
+
+  const [isValidLogin, setisValidLogin] = useState(false);
 
   const login = () => {
-    console.log('logging in!');
-  }
+    
+    props.setSignedIn(
+      emailText === props.user.email && passwordText === props.user.password);
+    setisValidLogin(true);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Sign In Screen</Text>
-      <Button
-        style={styles.button}
-        title="Sign In"
-        onPress={login}
-      />
+      <View style={styles.textBoxContainer}>
+        <TextInput
+          placeholder="Email"
+          onChangeText={handleEmailInput}
+          style={styles.textBox}
+          value={emailText}
+          underlineColorAndroid="transparent"
+        />
+        <TextInput
+          placeholder="Password"
+          onChangeText={handlePasswordInput}
+          value={passwordText}
+          style={styles.textBox}
+          underlineColorAndroid="transparent"
+          secureTextEntry={true}
+        />
+
+        <Button style={styles.button} title="Sign In" onPress={login} />
+        {isValidLogin && (
+          <View>
+            <Text style={styles.invalidCredentials}>
+              User email or password is incorrect
+            </Text>
+          </View>
+        )}
+      </View>
     </View>
   );
-};
+}
 
-// Getters: props.user.password OR props.user.username
+// Getters: props.user.password OR props.user.email
 function mapStateToProps(state) {
   return {
     user: state.user,
@@ -31,7 +75,8 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     testCommand: () => dispatch({ type: "PING" }), // console.log("pong");
-    setSignedIn: (isSignedIn) => dispatch({ type: "SET_SIGNED_IN", value: isSignedIn}),
+    setSignedIn: (isSignedIn) =>
+      dispatch({ type: "SET_SIGNED_IN", value: isSignedIn }),
   };
 }
 
@@ -43,10 +88,50 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  headerText: {
+    fontSize: 25,
+    textAlign: "center",
+    margin: 10,
+    color: "black",
+    fontWeight: "bold",
+  },
   button: {
     paddingHorizontal: 20,
     paddingVertical: 10,
     marginVertical: 10,
     borderRadius: 5,
+  },
+  textBox: {
+    fontSize: 20,
+    alignSelf: "stretch",
+    height: 45,
+    paddingRight: 45,
+    paddingLeft: 8,
+    borderWidth: 1,
+    paddingVertical: 0,
+    borderColor: color.grey,
+    borderRadius: 5,
+  },
+  textBoxContainer: {
+    position: "relative",
+    alignSelf: "stretch",
+    justifyContent: "center",
+  },
+  touchableButton: {
+    position: "absolute",
+    right: 3,
+    height: 40,
+    width: 35,
+    padding: 2,
+  },
+  buttonImage: {
+    resizeMode: "contain",
+    height: "100%",
+    width: "100%",
+  }, 
+  invalidCredentials: {
+    color: "red",
+    textAlign: "center",
+    marginTop: 5,
   },
 });
