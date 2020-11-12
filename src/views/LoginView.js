a513115298567b20a20f7655c5a5678f5f4ce72b
Andrew Raftovich <AndrewRaftovich@gmail.com>
2020-11-11 20:46:57 -0700

    Create registration page (#28)
    * Changed font size and added scroll view
    
    * Changed text input font and both password fields are now hidden
    
    * Updated error message on registration form
    
    Co-authored-by: Robbie Bagley <rbagley01@gmail.com>

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -8,12 +8,22 @@ import {
 import { Input, Image } from 'react-native-elements';
 import { connect } from "react-redux";
 import axios from 'axios';
+import { ScrollView, TextInput, TouchableOpacity } from "react-native-gesture-handler";
 
 function SigninView(props) {
   const [emailText, setEmailText] = useState("");
   const [passwordText, setPasswordText] = useState("");
   const [isInvalidLogin, setisInvalidLogin] = useState(false);
-
+  const [isLoginForm, setIsLoginForm] = useState(true);
+  const [emailReg, setEmailReg] = useState("");
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
+  const [confirm_passwordReg, setconfirm_passwordReg] = useState("");
+  const [first_nameReg, setfirst_nameReg] = useState("");
+  const [last_nameReg, setlast_nameReg] = useState("");
+  const [isInvalidRegistration, setIsInvalidRegistration] = useState(false);
+  const [InvalidRegistrationMessage, setInvalidRegistrationMessage] = useState("");
+  
   const login = () => {
     let formData = new FormData();
     // (Django won't let me rename 'username' to 'email')
@@ -23,11 +33,10 @@ function SigninView(props) {
     // Create request for user auth token
     axios.post(`${props.serverName}/login`, formData)
       .then(res => {
-
         // Set the user token in store
         props.setAuthToken(res.data.token);
 
-        // Once the auth token is recieved, access user data
+        // Once the auth token is received, access user data
         if (res.data.token) {
           const authKey = res.data.token;
           axios.get(`${props.serverName}/api/user`, {
@@ -60,14 +69,78 @@ function SigninView(props) {
       });
   }
 
+  const register = () => {
+    let formData = new FormData();
+    formData.append('email', emailReg);
+    formData.append('username', usernameReg);
+    formData.append('password', passwordReg);
+    formData.append('password2', confirm_passwordReg);
+    formData.append('first_name', first_nameReg);
+    formData.append('last_name', last_nameReg);
+
+    axios.post(`${props.serverName}/register`, formData)
+      .then((res) => {
+        props.setAuthToken(res.data.token);
+
+        // Once the auth token is received, access user data
+        if (res.data.token) {
+          const authKey = res.data.token;
+          axios.get(`${props.serverName}/messenger/user`, {
+            headers: {
+              'Authorization': `Token ${authKey}`
+            }
+          })
+            .then(res => {
+              // Save user data in store
+              props.setUserState(res.data);
+
+              axios.get(`${props.serverName}/messenger/messages/${props.selectedChatId}`, {
+                headers: {
+                  'Authorization': `Token ${authKey}`
+                }
+              })
+                .then((res) => {
+                  props.setMessages(res.data)
+                })
+                .catch((res) => console.log(res));
+              // Signin to program
+              props.setSignedIn(true);
+            })
+            .catch((res) => console.log(res));
+        }
+        else {
+          // Error occured in 200 response, display invalid fields to user
+          setIsInvalidRegistration(true);
+          let errorMessage = "Invalid: \n";
+          for (const [key, value] of Object.entries(res.data)) {
+
+            if (Array.isArray(value)) {
+              errorMessage += `${key.toString()}: ${value[0]}\n`
+            } else {
+              errorMessage += `${key.toString()}\n`
+            }
+          }
+          setInvalidRegistrationMessage(errorMessage);
+        }
+      })
+      .catch((res) => {
+        setIsInvalidRegistration(true);
+        setInvalidRegistrationMessage("Server issue encountered");
+      })
+  }
+
+
   return (
+    <ScrollView>
     <View style={styles.container}>
 
       <Image
         source={require('./../assets/smallTalk.png')}
         style={{ width: 400, height: 200 }}
       />
-
+      {isLoginForm ? (
+      <View style={{ flex: 1, position: "relative" }}>
+  
       <View style={styles.loginForm}>
         <Input
           placeholder="Email"
@@ -83,19 +156,106 @@ function SigninView(props) {
           secureTextEntry={true}
           onChangeText={(input) => setPasswordText(input)}
         />
-        <Button style={styles.loginButton} title="Sign In" onPress={login} />
+        <View style={styles.loginButton}>
+          <TouchableOpacity style={styles.homeScreenOpacity} onPress={login}>
+            <Text style={styles.buttonTextStyle}>
+              Sign In
+            </Text> 
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.loginButton}>
+          <TouchableOpacity style={styles.homeScreenOpacity} onPress={() => setIsLoginForm(false)}>
+            <Text style={styles.buttonTextStyle}>
+              Register Form
+            </Text> 
+          </TouchableOpacity>
+        </View>
       </View>
 
       <View style={{ flex: 1, position: "relative" }}>
         {isInvalidLogin && (
             <View>
               <Text style={styles.invalidCredentials}>
-                User email or password is incorrect
+                Invalid username or password
               </Text>
             </View>
           )}
       </View>
+      </View>) : (
+        <View style={{ flex: 1, position: "relative" }}>
+          <Text style={styles.inputStyle}>Email:</Text>
+          <TextInput  
+            placeholder="Email"
+            style={styles.textInput}
+            autoCorrect={false}
+            onChangeText={(input) => setEmailReg(input)}/> 
+
+          <Text style={styles.inputStyle}>Username:</Text>
+          <TextInput  
+            placeholder="username"
+            style={styles.textInput}
+            autoCorrect={false}
+            onChangeText={(input) => setUsernameReg(input)}/>  
+
+          <Text style={styles.inputStyle}>Password:</Text>
+          <TextInput  
+            placeholder="password"
+            style={styles.textInput}
+            autoCorrect={false}
+            secureTextEntry={true}
+            onChangeText={(input) => setPasswordReg(input)}/>  
+
+          <Text style={styles.inputStyle}>Confirm password:</Text>
+          <TextInput  
+            placeholder="confirm_password"
+            style={styles.textInput}
+            autoCorrect={false}
+            secureTextEntry={true}
+            onChangeText={(input) => setconfirm_passwordReg(input)}/>
+
+          <Text style={styles.inputStyle}>First name:</Text>
+          <TextInput  
+            placeholder="first_name"
+            style={styles.textInput}
+            autoCorrect={false}
+            onChangeText={(input) => setfirst_nameReg(input)}/>
+
+          <Text style={styles.inputStyle}>Last name:</Text>
+          <TextInput  
+            placeholder="last_name"
+            style={styles.textInput}
+            autoCorrect={false}
+            onChangeText={(input) => setlast_nameReg(input)}/>
+
+            <View style={styles.registerButton}>
+              <TouchableOpacity style={styles.homeScreenOpacity} onPress={() => register()}>
+                <Text style={styles.buttonTextStyle}>
+                  Register
+                </Text> 
+              </TouchableOpacity>
+            </View>
+            <View style={styles.loginButton}> 
+              <TouchableOpacity style={styles.homeScreenOpacity} onPress={() => setIsLoginForm(true)}>
+                <Text style={styles.buttonTextStyle}>
+                  Login Form
+                </Text> 
+              </TouchableOpacity>
+            </View>
+            <View style={{ flex: 1, position: "relative" }}>
+        {isInvalidRegistration && (
+            <View>
+              <Text style={styles.invalidCredentials}>
+                { InvalidRegistrationMessage }
+              </Text>
+            </View>
+          )}
+      </View>
+            
+        </View>
+      )}    
     </View>
+  </ScrollView>
   );
 }
 
@@ -136,20 +296,52 @@ const styles = StyleSheet.create({
   },
   loginForm: {
     position: "relative",
-    alignSelf: "stretch",
+    width: 350,
     justifyContent: "center",
-    marginHorizontal: 20,
+    marginHorizontal: 5,
     flex: 1,
   },
   inputStyle: {
-    
+    color: "black",
+    fontWeight: "bold", 
+    justifyContent: "center",
+    textAlign: "left",
+    fontSize: 18,
   },
   loginButton: {
     borderRadius: 5,
+    padding: 5,
+    alignItems: "center",
   },
   invalidCredentials: {
     color: "red",
     textAlign: "center",
     marginTop: 5,
+  },
+  registerButton: {
+    borderRadius: 5,
+    padding: 5,
+  },
+  homeScreenOpacity: {
+    backgroundColor: 'gray',
+    width: 152,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  buttonTextStyle: {
+    backgroundColor: "red",
+    color: "white",
+    borderColor: "black",
+    width: 150,
+    height: 30,
+    borderWidth: 1,
+    textAlign: "center",
+  },
+  textInput: {
+    color: "black",
+    fontWeight: "bold", 
+    justifyContent: "center",
+    textAlign: "left",
+    fontSize: 14,
   },
 });
